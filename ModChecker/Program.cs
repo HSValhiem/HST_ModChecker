ea28300818f5d211e792b91ef91cb15296a103d1
Salacious <chrisjenda@gmail.com>
2023-06-11 19:27:01 -0700

    Really Finished Now :)

--- ModChecker/Program.cs
+++ ModChecker/Program.cs
@@ -7,17 +7,15 @@ class Program
     {
         if (args.Length > 0)
         {
-            List<string> typeNamesToCheck;
             string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "typeNames.json");
             AssemblyDefinition ass = AssemblyDefinition.ReadAssembly(args[0]);
-            string found = "false";
+
+            List<string> typeNamesToCheck;
 
             try
             {
                 string jsonContent = File.ReadAllText(jsonFilePath);
-#pragma warning disable CS8600
-                typeNamesToCheck = JsonSerializer.Deserialize<List<string>>(jsonContent);
-#pragma warning restore CS8600
+                typeNamesToCheck = JsonSerializer.Deserialize<List<string>>(jsonContent) ?? new List<string>();
             }
             catch (Exception ex)
             {
@@ -31,15 +29,14 @@ class Program
                 };
             }
 
-            found = ass.MainModule.GetTypeReferences().Any(typeReference => typeNamesToCheck != null && typeNamesToCheck.Contains(typeReference.Name)) ? "true" : "false";
+            bool found = ass.MainModule.GetTypeReferences().Any(typeReference => typeNamesToCheck.Contains(typeReference.Name));
 
-            Console.Write(found);
-
+            Console.WriteLine(found ? "true" : "false");
         }
         else
         {
             Console.WriteLine("error: Please provide a file path as an argument.");
         }
-
     }
+
 }
